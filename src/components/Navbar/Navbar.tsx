41a4cccd0928f1fa9ee15c3aad0a45fef20b02fd
Jeels <joelhuaman7m@gmail.com>
2024-09-15 17:59:05 -0500

    feat: Add framer-motion dependency and implement responsive menu in Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,51 @@
-import React from 'react';
+import { useState } from 'react';
 import { NavbarMenu } from '../../mockData/data';
 import { CiSearch } from "react-icons/ci";
 import { MdMenu } from "react-icons/md";
 import { SiPhosphoricons } from "react-icons/si";
+import ResponsiveMenu from './ResponsiveMenu';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
         <nav>
-        <div className="container">
+        <div className="container flex justify-between items-center py-8">
             {/* Logo section */}
+            <div className="text-2xl flex items-center gap-2 font-bold uppercase">
+                <SiPhosphoricons />
+                <p className="text-primary">X App</p>
+            </div>
             {/* Menu section */}
+            <div className="hidden md:block">
+                <ul className="flex items-center gap-6 text-gray-600">
+                    {
+                        NavbarMenu.map((item) => {
+                            return (
+                                <li key={item.id}>
+                                    <a href={item.url} className='inline-block py-1 px-3 hover:text-primary font-semibold'>{item.title}</a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
             {/* Icons section */}
+            <div className='flex items-center gap-4'>
+                <button className='text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200'>
+                    <CiSearch />
+                </button>
+            </div>
             {/* Movile haburger Menu section */}
+            <div className='md:hidden' onClick={() => setIsOpen(!isOpen)}>
+                <MdMenu className='text-4xl'/>
+            </div>
         </div>
         </nav>
 
         {/* Mobile Sidebar section */}
+        <ResponsiveMenu open={isOpen}/>
     </>
   );
 };
